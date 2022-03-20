7efbe8d2fb31214ad21a63530fe042fb02b47b98
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2022-03-20 01:19:43 -0500

    chore: undo redirects

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   async redirects() {
     return [
-      {
-        source: '/',
-        destination: '/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders',
-        permanent: true,
-      },
+      // {
+      //   source: '/',
+      //   destination: '/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders',
+      //   permanent: true,
+      // },
     ]
   },
 }
