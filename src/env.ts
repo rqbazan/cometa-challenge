fbadd88e4b913516016bd1ae39f6a18c75d37409
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2022-03-20 10:30:46 -0500

    refactor: rename env var name

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 const env = {
   REMOTE_API_BASE_URL: process.env.REMOTE_API_BASE_URL as string,
-  REMOTE_API_HASH_HEADER: process.env.API_HASH_HEADER as string,
+  REMOTE_API_HASH_HEADER: process.env.REMOTE_API_HASH_HEADER as string,
   API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL as string,
 }
 
