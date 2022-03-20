e6739f3b7f89f5e8c8994db7fabd714f59d28b4c
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2022-03-20 00:00:48 -0500

    feat: add redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
