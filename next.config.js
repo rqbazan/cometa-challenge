ac91349a6367cd298ae5355e28bf4bd734463a79
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2022-03-20 20:34:35 -0500

    chore: add cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts'],
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+        ],
+      },
+    ]
+  },
   async rewrites() {
     return [
       {
